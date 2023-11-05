099285f2cca487ae82e33d2f0cdc3791dee34487
Andrej Staš <andrej.stas.net@gmail.com>
2023-11-05 21:18:42 +0100

    feat: add client logos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,28 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+// eslint-disable-next-line import/no-named-default
+import { default as NImage } from "next/image";
 import styles from "../styles/Home.module.css";
+
+const ImageWrapper = ({
+  src,
+  alt,
+  url,
+}: {
+  src: string;
+  alt: string;
+  url: string;
+}) => (
+  <a target="_blank" href={url} rel="noreferrer">
+    <NImage
+      className={styles.logo}
+      src={src}
+      alt={alt}
+      width={100}
+      height={40}
+    />
+  </a>
+);
 
 const Home: NextPage = () => (
   <div className={styles.container}>
@@ -26,6 +48,49 @@ const Home: NextPage = () => (
         </p>
       </section>
 
+      <h2 className={styles.sectionTitle}>Clients I Worked For</h2>
+      <section className={styles.clients}>
+        <div className={styles.logos}>
+          <ImageWrapper
+            src="/clients/astrazeneca.png"
+            alt="Astra Zeneca"
+            url="https://astrazeneca.com/"
+          />
+
+          <ImageWrapper
+            src="/clients/ebay.png"
+            alt="Ebay"
+            url="https://ebay.com/"
+          />
+
+          <ImageWrapper
+            src="/clients/msd.png"
+            alt="MSD"
+            url="https://www.msd.com/"
+          />
+
+          <ImageWrapper
+            src="/clients/renalytix.png"
+            alt="Renalytix"
+            url="https://renalytix.com/"
+          />
+
+          <ImageWrapper
+            src="/clients/treedom.png"
+            alt="treedom"
+            url="https://treedom.net/"
+          />
+
+          <ImageWrapper
+            src="/clients/vm.png"
+            alt="Virgin Media"
+            url="https://virginmedia.com/"
+          />
+        </div>
+      </section>
+
+      <h2 className={styles.sectionTitle}>Where To Go Next</h2>
+
       <div className={styles.grid}>
         <a
           target="_blank"
@@ -43,7 +108,7 @@ const Home: NextPage = () => (
           rel="noreferrer"
         >
           <h2>Stories.land &rarr;</h2>
-          <p>Travel blog</p>
+          <p>My travel blog</p>
         </a>
         <a
           target="_blank"
@@ -52,7 +117,7 @@ const Home: NextPage = () => (
           rel="noreferrer"
         >
           <h2>Willion.sk &rarr;</h2>
-          <p>Owner of Willion s.r.o.</p>
+          <p>My company</p>
         </a>
       </div>
     </main>
